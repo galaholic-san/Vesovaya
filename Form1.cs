a3aeb7ca46d3582dee56fe0b2552e508a3fdf84d
galaholic-san <72087480+galaholic-san@users.noreply.github.com>
2020-10-13 09:21:53 +0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WinApplication
+{
+    public partial class Form1 : Form
+    {
+        object obj = new object();
+        static private IntPtr openvision = OpenVision.CreateOpenVision("IPCamera", "0", "D:\\Video\\video.avi", 15, "D:\\Development\\WinApplication\\WinApplication\\Cascades\\license_plate.xml");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread display = new Thread(new ThreadStart(Display));
+            display.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void Display()
+        {
+            while (true)
+            {
+                lock (obj)
+                {
+                    OpenVision.DisplayImageFrame(openvision);
+                    Thread RecognizeThread = new Thread(new ThreadStart(Recognize));
+                    RecognizeThread.Start();
+
+                    RecognizeThread.Join();
+                }
+            }
+        }
+
+        public void Recognize()
+        {
+            OpenVision.PrepareImageTo(openvision);
+            byte[] buff = new byte[30];
+            OpenVision.RecognizePlate(openvision, buff);
+            if (buff.Length != 0)
+            {
+                string line = Encoding.Default.GetString(buff);
+                Debug.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ OpenVision.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace WinApplication
+{
+    class OpenVision
+    {
+        [DllImport("D:\\Development\\WinApplication\\WinApplication\\bin\\x64\\Debug\\openvision.dll", CallingConvention = CallingConvention.Cdecl)]
+        static public extern IntPtr CreateOpenVision(string title, string rtsp, 
+            string VideoPath, double fps, string HaarCascadePath);
+
+        [DllImport("D:\\Development\\WinApplication\\WinApplication\\bin\\x64\\Debug\\openvision.dll", CallingConvention = CallingConvention.Cdecl)]
+        static public extern void DisplayImageFrame(IntPtr obj);
+
+
+        [DllImport("D:\\Development\\WinApplication\\WinApplication\\bin\\x64\\Debug\\openvision.dll", CallingConvention = CallingConvention.Cdecl)]
+        static public extern void RecordImageFrame(IntPtr obj);
+
+
+        [DllImport("D:\\Development\\WinApplication\\WinApplication\\bin\\x64\\Debug\\openvision.dll", CallingConvention = CallingConvention.Cdecl)]
+        static public extern void PrepareImageTo(IntPtr obj);
+
+        [DllImport("D:\\Development\\WinApplication\\WinApplication\\bin\\x64\\Debug\\openvision.dll", CallingConvention = CallingConvention.Cdecl)]
+        static public extern void RecognizePlate(IntPtr obj, byte[] buff);
+    }
+}
